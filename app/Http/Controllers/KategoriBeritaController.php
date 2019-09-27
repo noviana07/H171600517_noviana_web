28dff0c5aad89ca48c88fce245692cef82e4c603
Noviana <noviana981107@gmail.com>
2019-09-27 05:44:01 -0700

    laravel tugas1-3

---
+++ app/Http/Controllers/KategoriBeritaController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\KategoriBerita;
+
+class KategoriBeritaController extends Controller
+{
+    public function index(){
+    //Eloquent => ORM (Object Relational Mapping)
+    $listKategoriBerita=KategoriBerita::all(); //select*from kategori_artikel
+
+    //blade
+    return view('kategori_berita.index', compact('listKategoriBerita'));
+    //return view(view: 'kategori_berita.index')->with('data',$listKategoriBerita);
+}
+}
